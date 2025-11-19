a7f96ff9128483fdd09030d97f37dc91ab7080da
ygorlima-py <ygor.limarsx@gmail.com>
2025-11-19 15:48:59 -0300

    Create models Enterprise, Plan, Role, Team

--- djangoapp/expenses/models.py
+++ djangoapp/expenses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Category(models.Model):
     class Meta:
@@ -274,8 +275,117 @@ class AlertRecused(models.Model):
     def __str__(self) -> str:
         return self.message # type: ignore
     
+class Plan(models.Model):
+    name = models.CharField(
+                    max_length=255,
+                    null=True,
+                    blank=True,
+    )
 
+    price = models.DecimalField(
+                decimal_places=2,
+                max_digits=10,
+                null=True,
+    )
+
+    description = models.TextField(blank=True)
+
+    max_users = models.IntegerField(default=1)
+
+    max_team = models.IntegerField(default=1)
+
+    status = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
     
+class EnterPrise(models.Model):
+    name = models.CharField(
+                    max_length=255,
+                    null=False,
+                    blank=False,
+    )
 
+    cnpj = models.CharField(
+                    max_length=18,
+                    unique=True,
+                    null=True,
+                    blank=True,
+    )
 
+    owner = models.ForeignKey(
+                            settings.AUTH_USER_MODEL,
+                            on_delete=models.CASCADE,
+                            related_name='enterprise'
+                            )
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    plan_type = models.ForeignKey(
+                        Plan,
+                        on_delete=models.SET_NULL,
+                        null=True,
+                        blank=True,
+                        )
+    
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Roles(models.Model):
+    name = models.CharField(
+                    max_length=100,
+                    null=True,
+                    blank=True,
+    )
+
+    description = models.TextField(blank=True)
+
+    hierarchy = models.IntegerField(default=1)
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Team(models.Model):
+    name = models.CharField(
+                    max_length=255,
+                    null=False,
+                    blank=False,
+    )
+
+    enterprise = models.ForeignKey(
+                    EnterPrise,
+                    on_delete=models.CASCADE,
+                    related_name='teams',
+    )
+
+    team_manager = models.ForeignKey(
+                    settings.AUTH_USER_MODEL,
+                    on_delete=models.SET_NULL,
+                    related_name='managed_teams',
+                    null=True,
+                    blank=True,
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
 
