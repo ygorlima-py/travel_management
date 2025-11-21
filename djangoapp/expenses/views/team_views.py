8d93d6d9b2798a4d8c6c35e5bd0f61f52b510427
ygorlima-py <ygor.limarsx@gmail.com>
2025-11-21 06:11:59 -0300

    Adjusting page team

--- djangoapp/expenses/views/team_views.py
+++ djangoapp/expenses/views/team_views.py
@@ -4,6 +4,8 @@ from expenses.models import Team, EnterPrise, Expenses
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
+
 
 @login_required(login_url='expense:login')
 def teams(request):
@@ -28,19 +30,21 @@ def team(request, team_id):
             .select_related('enterprise', 'team_manager'), pk=team_id
             )
         )
-
+    
     members = team.members.select_related('user')
     
     expenses = Expenses.objects.filter(
         owner_expenses=request.user,
         )
     
-    # calculation = Calculation(team, expenses).all()
+    page_number = request.GET.get("page")
+    paginator = Paginator(expenses, 25)  # Show 25 contacts per page.
+    page_obj = paginator.get_page(page_number)
 
     context = {
         'team': team,
-        'expenses': expenses,
         'members': members,
+        'page_obj': page_obj,
     }
 
     return render (
