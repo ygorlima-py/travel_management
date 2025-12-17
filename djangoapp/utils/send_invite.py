e753776ab78e337d720fe0d49c37e248c47e6753
ygorlima-py <ygor.limarsx@gmail.com>
2025-12-17 08:05:36 -0300

    Create template to invite member

--- djangoapp/utils/send_invite.py
+++ djangoapp/utils/send_invite.py
@@ -1,4 +1,5 @@
 from expenses.models import TeamInvite
+from django.template.loader import render_to_string
 from django.urls import reverse
 from django.conf import settings
 from django.core.mail import EmailMessage
@@ -9,26 +10,40 @@ class SendInvite:
         self.team_id = team_id
 
     def create_url(self):
-        invite = TeamInvite.objects.filter(
+        self.invite = TeamInvite.objects.filter(
             email=self.email,
             team_id=self.team_id,
             ).first()
         
-        token = invite.token # type: ignore
+        token = self.invite.token # type: ignore
         url = reverse('expense:accept_invite', args=[token])
         link = f'{settings.SITE_URL}{url}'
 
         return link
-        
-
-    def send_invite(self):
+    
+    def create_template(self):
         link = self.create_url()
 
+        # Render html:
+        html_content = render_to_string(
+            template_name='expenses/email/invite_template.html', 
+            context={
+                'invite': self.invite,
+                'link': link,
+            }
+        )
+        return html_content
+
+    def send_invite(self):
+        invite_html = self.create_template()
+
         email = EmailMessage(
-            subject='Você foi convidado para entrar em uma equipe',
-            body=f"Clique no link {link} para entrar na equipe",
+            subject=f'Convite para entrar na equipe {self.invite.team.name}',
+            body=invite_html,
             to=[self.email]
         )
+
+        email.content_subtype = "html"
         email.send()
 
         return True
