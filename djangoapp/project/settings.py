7f85fbdf0d250933aaa0bfd471abc2ec6419bce8
ygorlima-py <ygor.limarsx@gmail.com>
2026-01-03 17:18:44 -0300

    Change CRSF config and session cookie

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = [
     if h.strip()
     ]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
